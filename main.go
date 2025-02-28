ce9ad747e19ec40e2a49e99869c3abc6ebd21730
Nitin Rawat <146698019+rwtnitin@users.noreply.github.com>
2025-02-28 10:57:19 +0000

    Add the ability to shutdown server gracefully

--- main.go
+++ main.go
@@ -1,22 +1,46 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"log"
+	"net"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 var PORT string = "8080"
 
 func main() {
-	// Create a multiplexer
-	var mux *http.ServeMux = http.NewServeMux()
 
-	// Add routes to the multiplexer
+	var mux *http.ServeMux = http.NewServeMux()
 	mux.HandleFunc("GET /health", HealthHandler)
 
-	log.Println("Starting server on PORT", PORT)
-	if err := http.ListenAndServe(fmt.Sprintf(":%s", PORT), mux); err != nil {
-		log.Fatalf("Error starting server on PORT %s: %s\n", PORT, err)
+	var server *http.Server = &http.Server{
+		Addr:    net.JoinHostPort("", PORT),
+		Handler: mux,
 	}
+
+	var stop chan os.Signal = make(chan os.Signal, 1)
+
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		log.Println("Starting server on PORT", PORT)
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Error starting server on PORT %s: %s\n", PORT, err)
+		}
+	}()
+
+	var receivedSignal os.Signal = <-stop
+	log.Printf("\nReceived signal: '%s'. Shutting down gracefully...\n", receivedSignal)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Error shutting down server: %v\n", err)
+	}
+	log.Println("Server shut down successfully")
 }
